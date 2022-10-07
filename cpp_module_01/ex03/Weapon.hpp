ff7ad8772bf2c1dc3ed3ace04532196bb91e15ba
qin xia <qinxia@MacBook-Air-de-qin.local>
2022-10-07 17:36:46 +0200

    weapon

--- cpp_module_01/ex03/Weapon.hpp
+++ cpp_module_01/ex03/Weapon.hpp
@@ -3,10 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   Weapon.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qxia <qxia@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: qinxia <qinxia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 14:23:45 by qxia              #+#    #+#             */
-/*   Updated: 2022/10/07 14:23:46 by qxia             ###   ########.fr       */
+/*   Updated: 2022/10/07 17:35:53 by qinxia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#define WEAPON_HPP
+
+#include <iostream>
+
+class Weapon
+{
+private:
+    std::string type;
+
+public:
+    Weapon(std::string type);
+    ~Weapon();
+    std::string &getType(void) const;
+    void setType(std::string type);
+};
+
+#endif
