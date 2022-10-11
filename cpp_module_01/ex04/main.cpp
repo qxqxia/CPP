c414597e442e6c8daa7f4f4067c74f66c938c457
Qin XIA <qxia@e2r4p5.clusters.42paris.fr>
2022-10-11 10:40:57 +0200

    write/read

--- cpp_module_01/ex04/main.cpp
+++ cpp_module_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: qxia <qxia@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 15:21:45 by qxia              #+#    #+#             */
-/*   Updated: 2022/10/10 16:44:29 by qxia             ###   ########.fr       */
+/*   Updated: 2022/10/11 10:08:44 by qxia             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,30 @@
 #include <fstream> //open et close
 #include <string>
 
+int ft_replace(std::string s1, std::string s2, std::fstream &fs, std::string filename){
+        
+}
+
 int main(int ac, char **av){
     if (ac != 4){
         std::cout << "Please try again: ./REPLACE [filename] [s1] [s2]" << std::endl;
         return (1);
     } 
     std::string filename = av[1];
+    std::string replacefile = filename + ".replace";
     std::fstream fs;
     fs.open (av[1], std::fstream::in);
     if (fs.is_open()){
         std::string s1 = av[2]; //s1 not define
         std::string s2 = av[3]; //s2 not define
+        //if (ft_replace(s1, s2, fs, filename))
+            //return ERROR?;
     }
-    
-    
-    
-    fs.close();
-    return 0;
-    
+    else{
+        std::cout << "open [" << filename << "] failed !" << std::endl;
+        return (1);
+    };
+    return (0);    
+    //fs.close();
 }
 
