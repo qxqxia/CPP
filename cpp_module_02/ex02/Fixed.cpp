2a2f01ca3d1ecccf583aef8efe7e7cd3b61a59e0
Qin XIA <qxia@e1r5p9.clusters.42paris.fr>
2022-10-17 17:21:36 +0200

    ++

--- cpp_module_02/ex02/Fixed.cpp
+++ cpp_module_02/ex02/Fixed.cpp
@@ -6,9 +6,64 @@
 /*   By: qxia <qxia@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 14:42:38 by qxia              #+#    #+#             */
-/*   Updated: 2022/10/17 14:51:08 by qxia             ###   ########.fr       */
+/*   Updated: 2022/10/17 17:16:06 by qxia             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
+int const Fixed::bits = 8;
+
+Fixed::Fixed(void):valu(0){
+        return ;
+}
+
+Fixed::~Fixed(void){
+    return ;
+}
+
+Fixed::Fixed(Fixed const &src){
+    *this = src;
+    return ;
+}
+
+Fixed& Fixed::operator=(Fixed const &rhs){
+    this->value = rhs.getRawBits();
+    return *this;
+}
+
+int Fixed::getRawBits(void)const{
+    return this->value;
+}
+
+void Fixed::setRawBits(int const raw){
+    this->value = raw;
+    return ;
+}
+
+Fixed::Fixed(const int i):value(i << Fixed::bits){ // left shift 
+    return ;
+}
+
+Fixed::Fixed(const float f):value(roundf(f*(1 << Fixed::bits))){ 
+    return ;
+}
+
+float Fixed::toFloat( void ) const{
+    return ((float)this->getRawBits() / (1 << Fixed::bits));
+}
+
+int Fixed::toInt( void ) const{
+    return (this->getRawBits() / (1 << Fixed::bits));   
+}
+
+Fixed &Fixed::operator++(){
+    value ++;
+    return *this;
+}
+
+Fixed Fixed::operator++(int){
+    Fixed tmp(*this); //create un object temporaire
+    value ++;
+    return temp;
+}
--- cpp_module_02/ex02/Fixed.hpp
+++ cpp_module_02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: qxia <qxia@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 14:42:46 by qxia              #+#    #+#             */
-/*   Updated: 2022/10/17 16:41:35 by qxia             ###   ########.fr       */
+/*   Updated: 2022/10/17 17:20:03 by qxia             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,27 @@
 class Fixed{
     public:
         Fixed(void);
-        Fixed(const int i);
+        Fixed(const int i); //function overloading
         Fixed(const float f);
         Fixed(Fixed const &src); //cope
-        
-        Fixed& operator=(Fixed const &rhs)const; //operator overload
+        Fixed& operator=(Fixed const &rhs)const; //operator overload copy assignment
+       
         bool operator>(Fixed const &rhs)const;
         bool operator<(Fixed const &rhs)const;
         bool operator>=(Fixed const &rhs)const;
         bool operator<=(Fixed const &rhs)const;
         bool operator==(Fixed const &rhs)const;
         bool operator!=(Fixed const &rhs)const;
+        
         Fixed operator+(Fixed const &rhs)const;
         Fixed operator-(Fixed const &rhs)const;
         Fixed operator*(Fixed const &rhs)const;
         Fixed operator/(Fixed const &rhs)const;
-        Fixed operator++() //
+        
+        Fixed& operator++();//++i 前增量没有形参，而且是引用返回
+        Fixed operator++(int); //i++ 放上一个整数形参，就是后增量运行符，它是值返回
+        Fixed& operator--();
+        Fixed operator--(int);
         
         static Fixed& min(Fixed& num1, Fixed& num2);
         const static Fixed& min(const Fixed& num1, const Fixed& num2);
@@ -50,6 +55,6 @@ class Fixed{
         const static int bits;
 };
 
-std::ostream& operator << (std::ostream &o, Fixed const &in);
+std::ostream& operator << (std::ostream &os, Fixed const &obj); //insertion operator
 
 #endif
