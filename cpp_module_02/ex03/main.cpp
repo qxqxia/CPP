86f78dd32e0bc011538dee27177255c4dc7e8a17
Qin XIA <qxia@e1r4p16.clusters.42paris.fr>
2022-10-20 13:46:01 +0200

    bsp

--- cpp_module_02/ex03/main.cpp
+++ cpp_module_02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: qxia <qxia@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 15:03:39 by qxia              #+#    #+#             */
-/*   Updated: 2022/10/19 16:18:15 by qxia             ###   ########.fr       */
+/*   Updated: 2022/10/20 13:44:13 by qxia             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int main(){
     Point const p1(10, 0);
     Point const p2(30, 0);
     Point const p3(15, 15);
-    Point const o(12, 13);
+    Point const o(12, 1);
     std::cout << "p1 " << "(" << p1.getX().toFloat() << "," << p1.getY().toFloat() << ")\n";
     std::cout << "p2 " << "(" << p2.getX().toFloat() << "," << p2.getY().toFloat() << ")\n";
     std::cout << "p3 " << "(" << p3.getX().toFloat() << "," << p3.getY().toFloat() << ")\n";
