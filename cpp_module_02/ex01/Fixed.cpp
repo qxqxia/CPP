72d69c8fd3d6c5b17a4492dfcf6b4a3388f1206b
Qin XIA <qxia@e1r3p12.clusters.42paris.fr>
2022-10-21 12:22:24 +0200

    int

--- cpp_module_02/ex01/Fixed.cpp
+++ cpp_module_02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: qxia <qxia@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 11:17:26 by qxia              #+#    #+#             */
-/*   Updated: 2022/10/18 11:52:56 by qxia             ###   ########.fr       */
+/*   Updated: 2022/10/21 12:21:34 by qxia             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,6 @@ int Fixed::toInt( void ) const{
 }
 
 std::ostream& operator << (std::ostream &os, Fixed const &obj){
-    os << obj.toFloat(); //why toFloat
+    os << obj.toFloat(); 
     return os;
 }
