b09189770eb08f7e2016c6e50d5b35a60e59fb26
qin xia <qinxia@macbook-air-de-qin.home>
2022-11-01 22:14:05 +0100

    addidea

--- cpp_module_04/ex01/Cat.hpp
+++ cpp_module_04/ex01/Cat.hpp
@@ -3,29 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   Cat.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qxia <qxia@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: qinxia <qinxia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 13:12:30 by qxia              #+#    #+#             */
-/*   Updated: 2022/11/01 15:51:31 by qxia             ###   ########.fr       */
+/*   Updated: 2022/11/01 22:13:20 by qinxia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CAT_HPP
-# define CAT_HPP
+#define CAT_HPP
 
 #include "Animal.hpp"
+#include "Brain.hpp"
 
-class Cat : public Animal{
-    public:
-        Cat(void);
-        Cat(Cat const &src);
-        ~Cat(void);
+class Cat : public Animal
+{
+public:
+    Cat(void);
+    Cat(std::string name);
+    Cat(Cat const &src);
+    ~Cat(void);
 
-        Cat& operator=(Cat const &rhs);
-        void makeSound()const;
-        Brain* getBrain()const;
-    private:
-        Brain* CatBrain;
+    Cat &operator=(Cat const &rhs);
+    void makeSound() const;
+    Brain *getBrain() const;
+
+private:
+    Brain *CatBrain;
+    std::string _name;
 };
 
 #endif
