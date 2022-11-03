ef44737fd25ef0ee90772340d8db79c71599f86b
qin xia <qinxia@macbook-air-de-qin.home>
2022-11-03 22:53:31 +0100

    change commentaire

---
+++ ex01.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+	int a;
+};
+class B : virtual public A
+{
+public:
+	int b;
+};
+class C : virtual  public A
+{
+public:
+	int c;
+};
+class D : public B, public C
+{
+public:
+	int d;
+};
+int main()
+{
+	D d;
+	d.a = 5;
+	cout << d.a << endl;
+	return 0;
+}
