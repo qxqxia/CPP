3a0d66d44dd8c4841c22e3d482db13d363020317
Qin XIA <qxia@e1r3p12.clusters.42paris.fr>
2022-11-14 10:47:04 +0100

    ready

--- cpp_module_05/ex03/main.cpp
+++ cpp_module_05/ex03/main.cpp
@@ -1,71 +1,117 @@
-#include <string>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qxia <qxia@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/18 16:09:01 by xuwang            #+#    #+#             */
+/*   Updated: 2022/11/14 10:44:45 by qxia             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Intern.hpp"
 
-int	main(){
-	std::cout << "~~~~~~~ Intern tests ~~~~~~~\n";
-	Bureaucrat b1("b1", 80);
-	Bureaucrat b2("b2", 25);
-	std::cout << b1;
-	std::cout << b2;
-	std::cout << "\n";
+int main() {
 
-	Intern	someRandomIntern;
+    std::cout << "~~~~~~ShrubberyCreationForm 145, 137 tests~~~~~\n";
+     try {
+        Bureaucrat b1("b1", 120);
+        Intern someRandomIntern;
+        Form*   rrf;
 
-	Form*	rrf;
-	try {
-		rrf = someRandomIntern.makeForm("robotomy request", "RRF_Target");
-		b1.signForm(*rrf);
-		b1.executeForm(*rrf);
-		b2.signForm(*rrf);
-		b2.executeForm(*rrf);
-	} catch (std::exception &e) {
-		std::cout << e.what();
+        rrf = someRandomIntern.makeForm("shrubbery creation", "Bender");
+        if (rrf)
+        {
+            b1.signForm(*rrf);
+            b1.executeForm(*rrf);
+            delete rrf;
+        }
+    }catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
 	}
+    std::cout << std::endl;
+    try {
+        Bureaucrat b2("b2", 146);
+        Intern someRandomIntern;
+        Form*   rrf;
 
-	delete rrf;
-	std::cout << "\n";
-
-	Form*	scf;
-	try {
-		scf = someRandomIntern.makeForm("shrubbery creation", "SCF_Target");
-
-		b1.signForm(*scf);
-		b1.executeForm(*scf);
-		b2.signForm(*scf);
-		b2.executeForm(*scf);
-	} catch (std::exception &e) {
-		std::cout << e.what();
+        rrf = someRandomIntern.makeForm("shrubbery creation", "Bender_1");
+        if (rrf)
+        {
+            b2.signForm(*rrf);
+            b2.executeForm(*rrf);
+            delete rrf;
+        }
+    }catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
 	}
+    
+    std::cout << std::endl;
+    std::cout << "~~~~~~RobomyRequestForm 72, 45 tests~~~~~\n";
+    try {
+        Bureaucrat b3("b3", 30);
+        Intern someRandomIntern;
+        Form*   ddf;
 
-	delete scf;
-	std::cout << "\n";
-
-	Form*	ppf;
-	try {
-		ppf = someRandomIntern.makeForm("presidential pardon", "PPF_Target");
-
-		b1.signForm(*ppf);
-		b1.executeForm(*ppf);
-		b2.signForm(*ppf);
-		b2.executeForm(*ppf);
-	} catch (std::exception &e) {
-		std::cout << e.what();
+        ddf = someRandomIntern.makeForm("robotomy request", "bonjour");
+        if (ddf)
+        {
+            b3.signForm(*ddf);
+            b3.executeForm(*ddf);
+            delete ddf;
+        }
+    }catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
 	}
+    std::cout << std::endl;
+    try {
+        Bureaucrat b4("b4", 50);
+        Intern someRandomIntern;
+        Form*   ddf;
 
-	delete ppf;
-	std::cout << "\n";
-
-	Form*	naf;
-	try {
-		naf = someRandomIntern.makeForm("Not A Form", "NAF_Target");
-		b1.signForm(*naf);
-		b1.executeForm(*naf);
-		b2.signForm(*naf);
-		b2.executeForm(*naf);
-	} catch (std::exception &e) {
-		std::cout << e.what();
+        ddf = someRandomIntern.makeForm("robotomy request", "bonjour");
+        if (ddf)
+        {
+            b4.signForm(*ddf);
+            b4.executeForm(*ddf);
+            delete ddf;
+        }
+    }catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
 	}
-	delete naf;
+    std::cout << std::endl;
+    std::cout << "~~~~~~PresidentialPardonForm 25, 5 tests~~~~~\n";
+      try {
+        Bureaucrat b5("b5", 1);
+        Intern someRandomIntern;
+        Form*   fff;
 
-	return (0);
+        fff = someRandomIntern.makeForm("presidentialpardon", "hello");
+        if (fff)
+        {
+            b5.signForm(*fff);
+            b5.executeForm(*fff);
+            delete fff;
+        }
+    }catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+    std::cout << std::endl;
+    try {
+        Bureaucrat b6("b6", 10);
+        Intern someRandomIntern;
+        Form*   fff;
+
+        fff = someRandomIntern.makeForm("presidentialpardon", "hello");
+        if (fff)
+        {
+            b6.signForm(*fff);
+            b6.executeForm(*fff);
+            delete fff;
+        }
+    }catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+    return 0;
 }
