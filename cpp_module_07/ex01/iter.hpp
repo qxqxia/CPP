55cd2e025a3c53a2cc1783ed6d0a9c293691d07d
Qin XIA <qxia@e2r4p11.clusters.42paris.fr>
2022-11-17 17:49:27 +0100

    debug

--- cpp_module_07/ex01/iter.hpp
+++ cpp_module_07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: qxia <qxia@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 16:23:46 by qxia              #+#    #+#             */
-/*   Updated: 2022/11/17 17:11:47 by qxia             ###   ########.fr       */
+/*   Updated: 2022/11/17 17:46:47 by qxia             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@
 #include <iostream>
 
 template<typename T>
-void ft_element(T const &x){
+void ft_element(T &x){
     std::cout << x << std::endl;
 }
 
 template<typename T>
-void iter(T* array, T size, void   ){
-    
+void iter(T* array, T size, void ft_element<*array>(T const &x) ){
+    for (T i = 0; i < size; i++){
+        array[i];
+    }
 }
 
 #endif
--- cpp_module_07/ex01/main.cpp
+++ cpp_module_07/ex01/main.cpp
@@ -5,13 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: qxia <qxia@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/17 16:23:42 by qxia              #+#    #+#             */
-/*   Updated: 2022/11/17 16:24:27 by qxia             ###   ########.fr       */
+/*   Created: 2021/11/25 12:31:16 by xuwang            #+#    #+#             */
+/*   Updated: 2022/11/17 17:47:52 by qxia             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-int main(){
+int main() {
+    std::cout << "---print str---" << std::endl;
+    std::string str[] = {std::string("hello world"), std::string("goodbye")};
+    iter(str, 2, ft_element);
     
+    std::cout << "---print caractere---" << std::endl;
+    char c[] = {'a', 'b', 'c'};
+    iter(c, 3, f);
+
+    std::cout << "---print int---" << std::endl;
+    int i[] = {1, 2, 3, 4};
+    iter(i, 4, ft_element);
+    return 0;
 }
