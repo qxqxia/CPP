24b5b441368992ed34e164a14919b4bedb84d76b
qin xia <qinxia@macbook-air-de-qin.home>
2023-03-18 11:56:48 +0100

    bug not found

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -1,25 +1,78 @@
-# include <iostream>
-# include <stack>
-# include <sstream> //stringstream
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <sstream> //stringstream
+#include <cassert>
+using namespace std;
 
-/*using namespace std;
+// test stack
+/* stack<int> stack;
 
-int countWords(string str)
+stack.push(21);
+stack.push(22);
+stack.push(24);
+stack.push(25);
+
+int num = 28;
+stack.push(num);
+cout << stack.top() << " ";
+stack.pop();
+stack.pop();
+stack.pop();
+
+while (!stack.empty())
 {
-    stringstream s(str);
+    cout << stack.top() << " ";
+    stack.pop();
+}*/
 
-    string word;
+vector<string> SplitStr(string str)
+{
+    vector<string> v;
+    stringstream ss(str);
+    string s;
 
-    int n = 0;
-    while (s >> word)
-        n++;
-    return n;
+    while (ss >> s)
+        v.push_back(s);
+    return v;
 }
 
-int main()
+int CalculateRPN(vector<string> tokens)
 {
-    string s = "geeks for geeks geeks "
-               "contribution placements";
-    cout << "number of words are: " << countWords(s);
-    return 0;
-}*/
+    if (tokens.empty())
+        return 0;
+
+    stack<int> tmp;
+    for (size_t i = 0; i < tokens.size(); i++)
+    {
+        if (tokens[i].size() > 1 || (tokens[i].size() == 1 && tokens[i] >= "0" && tokens[i] <= "9"))
+            tmp.push(atoi(tokens[i].c_str()));
+        else
+        {
+            int b = tmp.top();
+            tmp.pop();
+            int a = tmp.top();
+            tmp.pop();
+            int res;
+            if (tokens[i] == "+")
+                res = a + b;
+            else if (tokens[i] == "-")
+                res = a - b;
+            else if (tokens[i] == "*")
+                res = a * b;
+            else if (tokens[i] == "/")
+                res = a / b;
+            tmp.push(res);
+        }
+    }
+    return tmp.top();
+}
+
+int main(int ac, char **av)
+{
+    if (ac == 2)
+    {
+        CalculateRPN(SplitStr(av[1]));
+        return 0;
+    }
+}
