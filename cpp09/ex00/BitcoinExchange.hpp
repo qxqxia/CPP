9433624be9c630b2d1a903c336f8f3a56e924ecc
qxqxia <qxqinxia@outlook.com>
2023-03-23 16:06:32 +0100

    upper-bound

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -5,6 +5,7 @@
 # include <map>
 # include <fstream> //ifstream and ofstream
 # include <math.h>  
+# include <sstream>
 
 # define YELLOW "\033[0;33m"
 # define GREEN "\033[0;32m"
@@ -13,5 +14,8 @@
 # define PRECISION 10 
 
 std::string remove_whitesp(std::string & line);
+template<typename T>
+std::string to_string(const T value);
+std::string to_string(double const value);
 
 #endif
